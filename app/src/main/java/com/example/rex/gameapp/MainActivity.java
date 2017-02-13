412ce7d566b9dea08d1feea7070997289ecbe3f0
Adam Fletcher <ziolanghirasu@gmail.com>
2017-02-13 13:20:32 -0700

    Testing Push Errors

--- app/src/main/java/com/example/rex/gameapp/MainActivity.java
+++ app/src/main/java/com/example/rex/gameapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     // This is a test of the push.
+    // I want pizza!!!!
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
