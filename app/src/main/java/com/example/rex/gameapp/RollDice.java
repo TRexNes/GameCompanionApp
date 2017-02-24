632454c4e27d86155bb7c986cdd2426a565ef56e
Adam Fletcher <ziolanghirasu@gmail.com>
2017-02-24 13:23:21 -0700

    Edited Roll Dice with variable and stub

--- app/src/main/java/com/example/rex/gameapp/RollDice.java
+++ app/src/main/java/com/example/rex/gameapp/RollDice.java
@@ -5,9 +5,18 @@ import android.os.Bundle;
 
 public class RollDice extends AppCompatActivity {
 
+    private int numDice;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.roll_dice);
+
+        numDice = 2;
+    }
+
+    void rollDice() {
+        // Loop through and random for each numDice.
+        // Display each result to the gridView.
     }
 }
