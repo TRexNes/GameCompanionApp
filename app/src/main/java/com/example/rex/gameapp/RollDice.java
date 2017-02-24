c23d8a8b898105946ffdf3c54a50be0cdec4cd09
Adam Fletcher <ziolanghirasu@gmail.com>
2017-02-24 13:15:47 -0700

    Added Roll Dice

---
+++ app/src/main/java/com/example/rex/gameapp/RollDice.java
@@ -0,0 +1,13 @@
+package com.example.rex.gameapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RollDice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.roll_dice);
+    }
+}
