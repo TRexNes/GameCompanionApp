73327291f2c2c76af98f09b50ac00e1a66b20355
Trex21 <nes14001@byui.edu>
2017-03-04 13:42:51 -0700

    player

--- app/src/main/java/com/example/rex/gameapp/Player.java
+++ app/src/main/java/com/example/rex/gameapp/Player.java
@@ -2,6 +2,8 @@ package com.example.rex.gameapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class Player extends AppCompatActivity {
 
@@ -9,6 +11,20 @@ public class Player extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
+    }
+
+    public void addPLayer(View view) {
+        EditText nameInput = (EditText) findViewById(R.id.new_player_name);
+        String name = nameInput.getText().toString();
+
+    }
+
+    public void removePlayer(View view) {
+
+    }
+
+    public void resetPlayer(View view) {
+
     }
 
 }
