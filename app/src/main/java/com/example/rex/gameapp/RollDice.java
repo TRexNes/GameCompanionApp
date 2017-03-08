aea6a7048091694ca8d358e2d827fa528d799fb1
Adam Fletcher <ziolanghirasu@gmail.com>
2017-03-08 12:50:59 -0700

    Updated RollDice

--- app/src/main/java/com/example/rex/gameapp/RollDice.java
+++ app/src/main/java/com/example/rex/gameapp/RollDice.java
@@ -25,59 +25,84 @@ public class RollDice extends AppCompatActivity {
         textView.setText("" + numDice);
     }
 
-    public void rollDice() {
+    public void rollDice(View view) {
         // Loop through and random for each numDice.
         // Display each result to the gridView.
-        int temp;
+        String temp;
         Random random = new Random();
-        TextView textView;
 
         for (int i = 1; i <= numDice; i++){
-            for (int j = 0; j < random.nextInt(10); j++) {
+            for (int j = 0; j < random.nextInt(6) + 3; j++) {
                 random.nextInt(6);
             }
 
-            temp = random.nextInt(6) + 1;
+            temp = String.valueOf(random.nextInt(6) + 1);
 
-            switch (i) {
-                case 1:
-                    textView = (TextView) findViewById(R.id.Die1);
-                    textView.setText("" + temp);
-                    break;
-                case 2:
-                    textView = (TextView) findViewById(R.id.Die2);
-                    textView.setText("" + temp);
-                    break;
-                case 3:
-                    textView = (TextView) findViewById(R.id.Die3);
-                    textView.setText("" + temp);
-                    break;
-                case 4:
-                    textView = (TextView) findViewById(R.id.Die4);
-                    textView.setText("" + temp);
-                    break;
-                case 5:
-                    textView = (TextView) findViewById(R.id.Die5);
-                    textView.setText("" + temp);
-                    break;
-                case 6:
-                    textView = (TextView) findViewById(R.id.Die6);
-                    textView.setText("" + temp);
-                    break;
-                case 7:
-                    textView = (TextView) findViewById(R.id.Die7);
-                    textView.setText("" + temp);
-                    break;
-                case 8:
-                    textView = (TextView) findViewById(R.id.Die8);
-                    textView.setText("" + temp);
-                    break;
-                case 9:
-                    textView = (TextView) findViewById(R.id.Die9);
-                    textView.setText("" + temp);
-                    break;
-            }
+            changeDice(i, temp);
         }
+    }
+
+    private void changeDice(int i, String temp) {
+        TextView textView;
+        switch (i) {
+            case 1:
+                textView = (TextView) findViewById(R.id.Die1);
+                textView.setText("" + temp);
+                break;
+            case 2:
+                textView = (TextView) findViewById(R.id.Die2);
+                textView.setText("" + temp);
+                break;
+            case 3:
+                textView = (TextView) findViewById(R.id.Die3);
+                textView.setText("" + temp);
+                break;
+            case 4:
+                textView = (TextView) findViewById(R.id.Die4);
+                textView.setText("" + temp);
+                break;
+            case 5:
+                textView = (TextView) findViewById(R.id.Die5);
+                textView.setText("" + temp);
+                break;
+            case 6:
+                textView = (TextView) findViewById(R.id.Die6);
+                textView.setText("" + temp);
+                break;
+            case 7:
+                textView = (TextView) findViewById(R.id.Die7);
+                textView.setText("" + temp);
+                break;
+            case 8:
+                textView = (TextView) findViewById(R.id.Die8);
+                textView.setText("" + temp);
+                break;
+            case 9:
+                textView = (TextView) findViewById(R.id.Die9);
+                textView.setText("" + temp);
+                break;
+        }
+    }
+
+    private void resetDice() {
+        TextView textView = (TextView) findViewById(R.id.Die1);
+        textView.setText("~");
+        textView = (TextView) findViewById(R.id.Die2);
+        textView.setText("~");
+        textView = (TextView) findViewById(R.id.Die3);
+        textView.setText("~");
+        textView = (TextView) findViewById(R.id.Die4);
+        textView.setText("~");
+        textView = (TextView) findViewById(R.id.Die5);
+        textView.setText("~");
+        textView = (TextView) findViewById(R.id.Die6);
+        textView.setText("~");
+        textView = (TextView) findViewById(R.id.Die7);
+        textView.setText("~");
+        textView = (TextView) findViewById(R.id.Die8);
+        textView.setText("~");
+        textView = (TextView) findViewById(R.id.Die9);
+        textView.setText("~");
     }
 
     public int getNumDice() {
@@ -91,6 +116,8 @@ public class RollDice extends AppCompatActivity {
 
         TextView textView = (TextView) findViewById(R.id.numDice);
         textView.setText("" + numDice);
+
+        resetDice();
     }
 
     public void removeDie(View view) {
@@ -100,5 +127,7 @@ public class RollDice extends AppCompatActivity {
 
         TextView textView = (TextView) findViewById(R.id.numDice);
         textView.setText("" + numDice);
+
+        resetDice();
     }
 }
